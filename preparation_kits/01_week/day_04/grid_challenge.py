077f0bba5196143836147c88b2e2d44f479d5a7f
lararli <larar.lopezi@outlook.com>
2023-03-29 20:00:52 -0300

    update grid challenge

--- preparation_kits/01_week/day_04/grid_challenge.py
+++ preparation_kits/01_week/day_04/grid_challenge.py
@@ -1,3 +1,20 @@
+"""
+The function takes a single argument grid,
+which is a list of strings representing a square grid of characters.
+It returns "YES" if it's possible to rearrange the grid such that
+each row and column is in non-decreasing order, and "NO" otherwise.
+
+The function first converts the grid of strings to a 2D list of
+characters using a list comprehension. Then it sorts each row in
+non-decreasing order using a simple for loop with row.sort().
+Finally, it checks each column by iterating over each column index j
+using range(len(grid[0])), and then comparing the current element in
+the current row with the previous element in the previous row using a
+nested for loop with i starting from 1. If a column is not in non-decreasing order,
+the function immediately returns "NO". If all rows and columns are in non-decreasing order,
+the function returns "YES".
+"""
+
 def gridChallenge(grid):
     """
     Determines if it's possible to rearrange the characters in a grid such that
